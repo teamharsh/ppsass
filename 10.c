0e4742c75f1808397e994175ec27f00cbe3c764a
HARSH <126059255+teamharsh@users.noreply.github.com>
2023-03-09 22:17:08 +0530

    Update 10.c

--- 10.c
+++ 10.c
@@ -1,1 +1,32 @@
 
+#include<stdio.h>
+
+int main()
+
+{
+
+ int a,b;
+
+ printf("Eter the numbers =\n");
+
+ scanf("\n%d\n%d",&a,&b);
+
+ 
+
+ a=a+b;
+
+ b=a-b; 
+
+ a=a-b;
+
+ 
+
+ printf("\na is %d",a);
+
+ printf("\nb is %d\n",b);
+
+ 
+
+ return 0;
+
+}
